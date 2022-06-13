a31c03b67b0ad436bce14957e45464b0a1bae090
mohammed ahmed <mohammed18200118@gmail.com>
2022-06-13 16:15:22 +0200

    initial commit

---
+++ task1/task1/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def addxy(request):
+    if(request.method == 'POST'):
+        x =int(request.POST.get('num1'))
+        y = int(request.POST.get('num2'))
+        z =x+y
+
+        # return HttpResponse("Result = " + str(z))
+        return render(request , 'index.html' ,{
+            'result' : str(z)
+        })
+
+    else:
+        return render(request , 'index.html')
